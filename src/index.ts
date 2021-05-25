fa3a2c732e44cfcc4e9c644153252c3c8014d10e
fadelkaadan <fadelkaadan@gmail.com>
2021-05-25 23:43:37 +0200

    save chat messages to dynamodb

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,45 @@
 import Discord, { Channel, Client, Message, TextChannel } from "discord.js";
+import AWS from "aws-sdk";
 import dotenv from "dotenv";
 import { start } from "./server";
 
 dotenv.config();
-const client: Client = new Discord.Client();
+AWS.config.update({
+  region: process.env.AWS_DEFAULT_REGION,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
 
-const minutes: number = 1000 * 60;
+// Create the service used to connect to DynamoDB
+const docClient = new AWS.DynamoDB.DocumentClient();
+const client: Client = new Discord.Client();
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user?.tag}!`);
 
-  setInterval(() => {
-    const channel: Channel | undefined = client.channels.cache.get(
-      `839657921553236020`
-    );
-    (channel as TextChannel)?.send("test");
-  }, minutes);
-
   client.on("message", (msg: Message) => {
     if (msg.author !== client.user) {
-      msg.reply(msg.author.username);
+      const params = {
+        TableName: "channel-messages",
+        Item: {
+          id: msg.id,
+          author: msg.author.username,
+          message: msg.content,
+          createdTimestamp: msg.createdTimestamp,
+        },
+      };
+
+      console.log("Adding a new item...");
+      docClient.put(params, function (err, data) {
+        if (err) {
+          console.error(
+            "Unable to add item. Error JSON:",
+            JSON.stringify(err, null, 2)
+          );
+        } else {
+          console.log("Added item:", JSON.stringify(data, null, 2));
+        }
+      });
     }
   });
 });

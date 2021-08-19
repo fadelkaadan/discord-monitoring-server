7b5cf6a543b1fc0aace7e4e960e3363c6cfc40e6
fadelkaadan <fadelkaadan@gmail.com>
2021-08-19 22:08:29 +0200

    Add a Messages controller

---
+++ src/controllers/messages.controller.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from "express";
+import { discord } from "../providers/discord";
+
+class messagesController {
+  getMessages = async (req: Request, res: Response) => {
+    try {
+      const messages = await discord.getMessages();
+
+      res.status(200).json({
+        status: "successfully fetched",
+        data: messages,
+      });
+    } catch (error) {
+      res.status(404).json({ error });
+    }
+  };
+
+  deleteMessage = async (req: Request, res: Response) => {
+    try {
+      const deletedMessage = discord.deleteMessage(req.params.id);
+
+      res.status(200).json({
+        status: "successfully deleted",
+        data: deletedMessage,
+      });
+    } catch (error) {
+      res.status(400).json({ error });
+    }
+  };
+}
+
+export default new messagesController();

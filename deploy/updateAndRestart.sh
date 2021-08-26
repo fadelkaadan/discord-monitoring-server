d549a29cc57a4c9d4c08e2be345e979a3fd9efd2
fadelkaadan <fadelkaadan@gmail.com>
2021-08-26 13:55:05 +0200

    Remove test stage

--- deploy/updateAndRestart.sh
+++ deploy/updateAndRestart.sh
@@ -10,14 +10,6 @@ git clone https://gitlab.com/fadelkaadan/discord-monitoring-server.git
 
 export PATH=/home/ubuntu/node/bin:$PATH
 
-# stop the previous pm2
-pm2 kill
-npm remove pm2 -g
-
-
-#pm2 needs to be installed globally as we would be deleting the repo folder.
-# this needs to be done only once as a setup script.
-npm install pm2 -g
 # starting pm2 daemon
 pm2 status
 

960ba2aa8e9d9c63064789647da10f8bf44f276f
fadelkaadan <fadelkaadan@gmail.com>
2021-08-26 13:13:59 +0200

    Change user

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -15,5 +15,5 @@ echo "ALL_SERVERS ${ALL_SERVERS}"
 for server in "${ALL_SERVERS[@]}"
 do
   echo "deploying to ${server}"
-  ssh ec2-user@${server} 'bash' < ./deploy/updateAndRestart.sh
+  ssh ubuntu@${server} 'bash' < ./deploy/updateAndRestart.sh
 done

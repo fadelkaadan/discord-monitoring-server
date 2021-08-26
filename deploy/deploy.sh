33e287a07e57ae8c7a821ea8440fbc188a64d294
fadelkaadan <fadelkaadan@gmail.com>
2021-08-26 12:36:55 +0200

    Adjust deploy sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,15 +5,15 @@ set -e
 eval $(ssh-agent -s)
 echo "$PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
 
-./disableHostKeyChecking.sh
+./deploy/disableHostKeyChecking.sh
 
-DEPLOY_SERVERS=$DEPLOY_SERVERS
+DEPLOY_SERVER=$DEPLOY_SERVER
 
-ALL_SERVERS=(${DEPLOY_SERVERS//,/ })
+ALL_SERVERS=(${DEPLOY_SERVER//,/ })
 echo "ALL_SERVERS ${ALL_SERVERS}"
 
 for server in "${ALL_SERVERS[@]}"
 do
   echo "deploying to ${server}"
-  ssh ubuntu@${server} 'bash' < ./updateAndRestart.sh
+  ssh ubuntu@${server} 'bash' < ./deploy/updateAndRestart.sh
 done
